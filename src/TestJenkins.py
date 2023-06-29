0f509cf1cc8c815c93125da211fa8ba5f269bef0
uttamraj9 <uttamraj9@gmail.com>
2023-06-29 16:14:04 +0100

    My first commit

---
+++ src/TestJenkins.py
@@ -0,0 +1,11 @@
+# Define a list of numbers
+numbers = [5, 2, 9, 11, 8]
+
+# Calculate the sum of the numbers
+total = sum(numbers)
+
+# Calculate the average
+average = total / len(numbers)
+
+# Print the average
+print("The average is:", average)
